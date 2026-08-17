1ef9b5a926d957295fe57f9b601b10ee0e47c5f4
Monami <monami.saeki@gmail.com>
2026-08-17 15:51:53 +0300

    completed exercise 16

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -5,7 +5,7 @@ const { createClient } = require("redis");
 
 let client;
 const initRedis = async () => {
-  client = createClient();
+  client = createClient({ url: process.env.REDIS_URL });
   client.on("error", (err) => console.log("Redis Client Error", err));
   await client.connect();
 };
