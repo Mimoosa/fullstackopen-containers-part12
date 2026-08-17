25f2e3b5d51e49321741bf28401b249fd4f715b8
Monami <monami.saeki@gmail.com>
2026-08-17 12:52:25 +0300

    completed exercise 15

--- todo-app/todo-frontend/vite.config.js
+++ todo-app/todo-frontend/vite.config.js
@@ -9,4 +9,10 @@ export default defineConfig({
     globals: true,
     setupFiles: "./testSetup.js",
   },
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+  },
 });
