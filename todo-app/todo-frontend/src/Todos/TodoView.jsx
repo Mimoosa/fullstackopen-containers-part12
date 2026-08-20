4641ca6207af47e6fecc84b5c25b0f13f9369951
Monami <monami.saeki@gmail.com>
2026-08-20 23:58:51 +0300

    completed exercise 20

--- todo-app/todo-frontend/src/Todos/TodoView.jsx
+++ todo-app/todo-frontend/src/Todos/TodoView.jsx
@@ -7,8 +7,14 @@ import Form from "./Form";
 const TodoView = () => {
   const [todos, setTodos] = useState([]);
 
+  console.log("todos from API:", todos);
+  console.log("type:", typeof todos);
+  console.log("isArray:", Array.isArray(todos));
+
   const refreshTodos = async () => {
-    const { data } = await axios.get("/todos");
+    const { data } = await axios.get(
+      `${import.meta.env.VITE_BACKEND_URL}/todos`,
+    );
     setTodos(data);
   };
 
@@ -17,17 +23,20 @@ const TodoView = () => {
   }, []);
 
   const createTodo = async (todo) => {
-    const { data } = await axios.post("/todos", todo);
+    const { data } = await axios.post(
+      `${import.meta.env.VITE_BACKEND_URL}/todos`,
+      todo,
+    );
     setTodos([...todos, data]);
   };
 
   const deleteTodo = async (todo) => {
-    await axios.delete(`/todos/${todo._id}`);
+    await axios.delete(`${import.meta.env.VITE_BACKEND_URL}/todos/${todo._id}`);
     refreshTodos();
   };
 
   const completeTodo = async (todo) => {
-    await axios.put(`/todos/${todo._id}`, {
+    await axios.put(`${import.meta.env.VITE_BACKEND_URL}/todos/${todo._id}`, {
       text: todo.text,
       done: true,
     });
@@ -36,7 +45,7 @@ const TodoView = () => {
 
   return (
     <>
-      <h1>Todos</h1>
+      <h1>Todos App</h1>
       <Form createTodo={createTodo} />
       <List todos={todos} deleteTodo={deleteTodo} completeTodo={completeTodo} />
     </>
